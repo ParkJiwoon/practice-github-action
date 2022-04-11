c98bbe2a32327ad8292c72d48142429196f20cb8
ParkJiwoon <bcp0109@gmail.com>
2022-04-12 02:21:47 +0900

    test: Sample Test 추가

---
+++ src/test/kotlin/com/example/practicegithubaction/SampleTest.kt
@@ -0,0 +1,17 @@
+package com.example.practicegithubaction
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+
+    @Test
+    fun testSuccess() {
+        assertThat("hello").isEqualTo("hello")
+    }
+
+    @Test
+    fun testFail() {
+        assertThat("hello").isEqualTo("hi")
+    }
+}
