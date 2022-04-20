fa6189a3b897f3d3f2a0c56c4f1343f8948403ee
ParkJiwoon <bcp0109@gmail.com>
2022-04-20 21:27:21 +0900

    deploy.sh 스크립트에서 sudo 제거

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # docker image 빌드
-sudo docker build -t springboot/sample .
+docker build -t springboot/sample .
 
 # 기존에 떠 있는 도커 컨테이너 종료
-sudo docker rm -f codedeploy
+docker rm -f codedeploy
 
 # 도커 이미지 삭제
-sudo docker rmi springboot/sample
+docker rmi springboot/sample
 
 # docker 이미지 실행
-sudo docker run -d --name codedeploy -p 8080:8080 springboot/sample
+docker run -d --name codedeploy -p 8080:8080 springboot/sample
