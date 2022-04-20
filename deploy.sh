6bee119745093f5ff2e4039eec0d5862aeb29dd5
ParkJiwoon <bcp0109@gmail.com>
2022-04-20 19:33:00 +0900

    feat: ECR 사용할 필요가 없어서 workflow 에서 제거
    - EC2 에서 바로 도커 이미지 빌드 후 실행하도록 변경

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
-# AWS ECR 에서 도커 이미지 가져오기 위해 인증
-(aws ecr get-login --region ap-northeast-2 --no-include-email)
+# docker image 빌드
+sudo docker build -t springboot/sample .
 
 # 기존에 떠 있는 도커 컨테이너 종료
 sudo docker rm -f codedeploy
 
 # 도커 이미지 삭제
 sudo docker rmi springboot/sample
-
-# 최신 도커 이미지 가져오기
-sudo docker pull springboot/sample:latest
 
 # docker 이미지 실행
 sudo docker run -d --name codedeploy -p 8080:8080 springboot/sample
