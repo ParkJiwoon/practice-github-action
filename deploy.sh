4436c4cae6723deba8f76cfde0c1c2a49fb11fa5
ParkJiwoon <bcp0109@gmail.com>
2022-04-20 21:17:49 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo "hello world"
+# docker image 빌드
+sudo docker build -t springboot/sample .
 
-## docker image 빌드
-#sudo docker build -t springboot/sample .
-#
-## 기존에 떠 있는 도커 컨테이너 종료
-#sudo docker rm -f codedeploy
-#
-## 도커 이미지 삭제
-#sudo docker rmi springboot/sample
-#
-## docker 이미지 실행
-#sudo docker run -d --name codedeploy -p 8080:8080 springboot/sample
+# 기존에 떠 있는 도커 컨테이너 종료
+sudo docker rm -f codedeploy
+
+# 도커 이미지 삭제
+sudo docker rmi springboot/sample
+
+# docker 이미지 실행
+sudo docker run -d --name codedeploy -p 8080:8080 springboot/sample
