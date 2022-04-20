58a8f2d9758b1c46c67cf5237c4e368e10fa3bc4
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 00:23:21 +0900

    배포명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-REPOSITORY = /home/ec2-user
-PROJECT_NAME = app
+REPOSITORY="/home/ec2-user"
+PROJECT_NAME="app"
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID = $(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -f $PROJECT_NAME | grep java | awk '{print $1}')
 
 # 프로세스가 켜져 있으면 죽이기
 if [ -z "$CURRENT_PID" ]; then
-  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   kill -15 $CURRENT_PID
   sleep 5
