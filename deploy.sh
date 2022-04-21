b8a58d5094d9a5350f264c4ccfadaa005538c3cb
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 17:03:38 +0900

    deploy: echo 코드 삭제..

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,7 @@ CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 # 프로세스가 켜져 있으면 죽이기
 if [ -z $CURRENT_PID ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> ./error_deploy.log
 else
-  echo "> 현재 실행중인 애플리케이션 강제 종료 $CURRENT_PID" >> ./error_deploy.log
   kill -15 $CURRENT_PID
 fi
 
