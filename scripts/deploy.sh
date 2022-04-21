f5b49694b07c430512aa32b52ce7aba1f251d78d
ParkJiwoon <bcp0109@gmail.com>
2022-04-22 01:40:15 +0900

    deploy: log 파일들 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+
+$APP_LOG="$PROJECT_ROOT/logs/application.log"
+$ERROR_LOG="$PROJECT_ROOT/logs/error.log"
+$DEPLOY_LOG="$PROJECT_ROOT/logs/deploy.log"
+
 #JAR_FILE="app.jar"
 #
 ## 현재 구동 중인 애플리케이션 pid 확인
@@ -14,12 +19,12 @@ JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 #fi
 
 # build 파일 복사
-echo "> 파일 복사" >> success_deploy.log
+echo "> 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-echo "> 파일 실행" >> success_deploy.log
-nohup java -jar $JAR_FILE &
+echo "> 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "> 실행된 프로세스 아이디 $CURRENT_PID" >> success_deploy.log
+echo "> 실행된 프로세스 아이디 $CURRENT_PID" >> $DEPLOY_LOG
