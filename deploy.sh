ed261584a0dcf1dd2ad2c03c7baf8684e2fceaee
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 17:28:40 +0900

    deploy: 단순하게 앱 실행하는 걸로 변경

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="app.jar"
-
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-# 프로세스가 켜져 있으면 죽이기
-if [ -z $CURRENT_PID ]; then
-else
-  kill -15 $CURRENT_PID
-fi
+#PROJECT_ROOT="/home/ubuntu/app"
+#JAR_FILE="app.jar"
+#
+## 현재 구동 중인 애플리케이션 pid 확인
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
+#
+## 프로세스가 켜져 있으면 죽이기
+#if [ -z $CURRENT_PID ]; then
+#else
+#  kill -15 $CURRENT_PID
+#fi
 
 # build 파일 복사
-cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT/$JAR_FILE
+cp ./build/libs/*.jar app.jar
 
 # jar 파일 실행
-nohup java -jar $PROJECT_ROOT/$JAR_FILE &
+nohup java -jar app.jar &
