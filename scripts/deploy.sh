5d561a41f8d29a227439fb4d7b8ddbada5218880
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 16:33:55 +0900

    deploy: 배포 로그 추가 및 파일 위치 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,8 @@ CURRENT_PID=$(pgrep -f $JAR_FILE)
 if [ -z $CURRENT_PID ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> ./error_deploy.log
 else
+  echo "> 현재 실행중인 애플리케이션 강제 종료 $CURRENT_PID" >> ./error_deploy.log
   kill -15 $CURRENT_PID
-  sleep 5
 fi
 
 # build 파일 복사
@@ -19,3 +19,7 @@ cp ./build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 nohup java -jar $JAR_FILE --server.port=8081 &
+
+sleep 3
+
+echo "> 배포 완료 $(pgrep -f $JAR_FILE)" >> ./error_deploy.log
