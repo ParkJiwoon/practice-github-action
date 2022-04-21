a0ed2ba4103c402985880585a84659c033f82c1b
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 12:22:55 +0900

    배포파일 수정 (jar 파일 명시적으로)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 REPOSITORY="/home/ubuntu"
-PROJECT_NAME="app"
+JAR_FILE="app.jar"
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 # 프로세스가 켜져 있으면 죽이기
 if [ -z $CURRENT_PID ]; then
@@ -15,10 +15,7 @@ else
 fi
 
 # build 파일 복사
-cp ./build/libs/*.jar "$PROJECT_NAME.jar"
-
-# jar 파일
-JAR_FILE="$REPOSITORY/$PROJECT_NAME.jar"
+cp ./build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 nohup java -jar $JAR_FILE > ./nohup.out 2>&1 &
