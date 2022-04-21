7087ea5e0eb088f5dee45b0906bb0f6a974f62ad
ParkJiwoon <bcp0109@gmail.com>
2022-04-21 23:26:15 +0900

    deploy: 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROJECT_NAME="spring-webapp"
+JAR_FILE="$PROJECT_NAME.jar"
 #PROJECT_ROOT="/home/ubuntu/app"
 #JAR_FILE="app.jar"
 #
@@ -14,11 +16,11 @@
 
 # build 파일 복사
 echo "> 파일 복사" >> susccess_deploy.log
-cp ./build/libs/*.jar app.jar
+cp ./build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "> 파일 실행" >> susccess_deploy.log
-nohup java -jar app.jar &
+nohup java -jar $JAR_FILE &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "> 실행된 프로세스 아이디 $CURRENT_PID" >> susccess_deploy.log
