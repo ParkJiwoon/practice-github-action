8d9a3d538305e71a372398bea68c1c7463a85a9d
ParkJiwoon <bcp0109@gmail.com>
2022-04-22 02:11:41 +0900

    deploy: 테스트를 위해 echo 만 남기기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,11 +20,11 @@ $DEPLOY_LOG="$PROJECT_ROOT/logs/deploy.log"
 
 # build 파일 복사
 echo "> 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "> 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+#nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "> 실행된 프로세스 아이디 $CURRENT_PID" >> $DEPLOY_LOG
