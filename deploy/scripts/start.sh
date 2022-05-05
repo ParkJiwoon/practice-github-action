d992f05aaf0989f55a78e9637472850e2d10ef8a
ParkJiwoon <bcp0109@gmail.com>
2022-05-05 16:46:39 +0900

    전체 프로젝트 코드 대신 빌드한 JAR 파일만 AWS 에 배포하기

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -9,10 +9,6 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
